e36f6f9e500e661d547536f9345102546222f3db
ramon-richard <ramon.richard@iisgalvanimi.edu.it>
2022-01-09 13:40:51 +0000

    fine

--- provaApp/src/app/app.component.ts
+++ provaApp/src/app/app.component.ts
@@ -10,6 +10,13 @@ export class AppComponent {
   articles = new Array<string>();
   addSpam(testo : HTMLInputElement,numero : HTMLInputElement): boolean
 {
+  let t = testo.value;
+  let n = Number(numero.value);
+
+  for (let index = 0; index < n; index++) {
+    this.articles.push(t)
+    
+  }
 return false;
   }
 }
